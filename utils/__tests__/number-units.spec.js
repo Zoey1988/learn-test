bfca0c0666a04c6dddf9d39368fc60a6f8e9bc5e
Zeinab <zeinab.rezaee67@gmail.com>
2021-12-08 11:38:28 +0330

    test: add number-units test

--- utils/__tests__/number-units.spec.js
+++ utils/__tests__/number-units.spec.js
@@ -2,15 +2,22 @@ import { withUnits } from '..';
 
 // 🧭🧭 time: 10 minutes
 
-// 💰 check if it returns { count: '5', unit:'k'} for 5000
-// 💰 check if it returns { count: '50', unit:'k'} for 50000
-// 💰 check if it returns { count: '500', unit:'k'} for 500000
-// 💰 check if it returns { count: '5', unit:'m'} for 5000000
+// 💰 check if it returns { count: '5', unit:'K'} for 5000
+// 💰 check if it returns { count: '50', unit:'K'} for 50000
+// 💰 check if it returns { count: '500', unit:'K'} for 500000
+// 💰 check if it returns { count: '5', unit:'M'} for 5000000
 // 💰 check if it returns NaN for 'sss'
-// 💰 check if it returns { count: '5', unit:'m'} for 5000000
 
 // useful links
 // https://jestjs.io/docs/expect#toequalvalue
 // https://jestjs.io/docs/expect#tobenan
 
 // code here 🔥
+
+test('number units', () => {
+  expect(withUnits(5000, 2)).toEqual({ count: '5', unit: 'K' });
+  expect(withUnits(50000, 2)).toEqual({ count: '50', unit: 'K' });
+  expect(withUnits(500000, 2)).toEqual({ count: '500', unit: 'K' });
+  expect(withUnits(5000000)).toEqual({ count: '5', unit: 'M' });
+  expect(withUnits('sss')).toBeNaN();
+});
